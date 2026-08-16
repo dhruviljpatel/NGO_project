b640b9337897e841829ba73740ee07fc585b3c93
dhruviljpatel <dhruviljpatel1@gmail.com>
2026-08-16 16:20:53 +0530

    config added vite-api-url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,34 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss()
-    ],
-    resolve: {
-        alias: {
-            "@": path.resolve(import.meta.dirname, "./src"),
-        },
-    },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:5000',
-                changeOrigin: true,
+export default defineConfig(({ mode }) => {
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            react(),
+            tailwindcss()
+        ],
+        resolve: {
+            alias: {
+                "@": path.resolve(import.meta.dirname, "./src"),
             },
         },
-    },
-    preview: {
-        allowedHosts: ["ngo-project-jrb9.onrender.com"],
-    },
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.vite_api_url,
+                    changeOrigin: true,
+                },
+            },
+        },
+        preview: {
+            allowedHosts: ["ngo-project-jrb9.onrender.com"],
+        },
+    }
 })
