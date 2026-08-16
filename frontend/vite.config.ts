383be109eed6b254577dcd40c5e973d7dae6116b
dhruviljpatel <dhruviljpatel1@gmail.com>
2026-08-16 17:34:15 +0530

    fixed bugs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,34 +1,20 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-    // Load env file based on `mode` in the current working directory.
-    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-    const env = loadEnv(mode, process.cwd(), '');
-
-    return {
-        plugins: [
-            react(),
-            tailwindcss()
-        ],
-        resolve: {
-            alias: {
-                "@": path.resolve(import.meta.dirname, "./src"),
-            },
+export default defineConfig({
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(import.meta.dirname, "./src"),
         },
-        server: {
-            proxy: {
-                '/api': {
-                    target: env.VITE_API_URL,
-                    changeOrigin: true,
-                },
-            },
-        },
-        preview: {
-            allowedHosts: ["ngo-project-jrb9.onrender.com"],
-        },
-    }
+    },
+    preview: {
+        allowedHosts: ["ngo-project-jrb9.onrender.com"],
+    },
 })
